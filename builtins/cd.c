110b6ffc869088c5de2f92cacdba583678412dbd
samymessal <samy.messal.sm@gmail.com>
2023-01-06 14:40:45 +0100

    cd

--- builtins/cd.c
+++ builtins/cd.c
@@ -3,11 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zel-kass <zel-kass@student.42.fr>          +#+  +:+       +#+        */
+/*   By: smessal <smessal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/05 17:19:40 by zel-kass          #+#    #+#             */
-/*   Updated: 2023/01/05 17:20:13 by zel-kass         ###   ########.fr       */
+/*   Updated: 2023/01/06 14:40:31 by smessal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
+#include "../includes/minishell.h"
+
+int    change_dir(char *path)
+{
+    int success;
+
+    success = chdir(path);
+    return(success);
+}
