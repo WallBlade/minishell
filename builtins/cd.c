44832229fe5700932ba37944085d4e37c5f4fcc0
samymessal <samy.messal.sm@gmail.com>
2023-01-06 14:41:08 +0100

    cd2

--- builtins/cd.c
+++ builtins/cd.c
@@ -6,11 +6,11 @@
 /*   By: smessal <smessal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/05 17:19:40 by zel-kass          #+#    #+#             */
-/*   Updated: 2023/01/06 14:40:31 by smessal          ###   ########.fr       */
+/*   Updated: 2023/01/06 14:40:57 by smessal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/minishell.h"
+#include "minishell.h"
 
 int    change_dir(char *path)
 {
